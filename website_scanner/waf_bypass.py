1ff54d0a4c22379aab00fe2098fc031a46a43b1e
》N̶̸E̶̴L̶̴ ̈́̕L̶̸A̶̴《 <n3ll4@protonmail.com>
2024-07-25 16:20:51 +0200

    Update waf_bypass.py
    waf paras

--- website_scanner/waf_bypass.py
+++ website_scanner/waf_bypass.py
@@ -16,18 +16,33 @@ res = Style.RESET_ALL
 
 def test_waf_bypass(domain):
     payloads = ["<script>alert('XSS')</script>", "' OR '1'='1"]
+    query_params = [
+        "test", "id", "name", "search", "q", "query", "item", "category",
+        "product", "page", "user", "username", "email", "login", "pass",
+        "password", "type", "sort", "order", "filter", "view", "action",
+        "cmd", "command", "module", "path", "file", "filename", "dir",
+        "directory", "folder", "content", "data", "date", "time", "year",
+        "month", "day", "event", "title", "description", "comment", "message",
+        "post", "article", "news", "blog", "forum", "thread", "topic", "board",
+        "section", "chapter", "pageid", "postid", "threadid", "articleid",
+        "newsid", "blogid", "forumid", "topicid", "boardid", "sectionid",
+        "chapterid"
+    ]
+
     options = Options()
     options.headless = True
     driver = webdriver.Firefox(options=options)
+    
     try:
         for payload in payloads:
-            test_url = f"{domain}?test={payload}"
-            driver.get(test_url)
-            if payload in driver.page_source:
-                print(f"{fg}[+] WAF bypassed with payload: {payload}{fw}")
-            else:
-                print(f"{fr}[-] Payload not detected: {payload}{fw}")
+            for param in query_params:
+                test_url = f"{domain}/?{param}={payload}"
+                driver.get(test_url)
+                if payload in driver.page_source:
+                    print(f"{fg}{bd}[+] WAF bypassed with payload: {payload} on parameter: {param}{res}")
+                else:
+                    print(f"{fr}{bd}[-] Payload not detected: {payload} on parameter: {param}{res}")
     except Exception as e:
-        print(f"{fr}[-] Error testing WAF bypass: {e}{fw}")
+        print(f"{fr}{bd}[-] Error testing WAF bypass: {e}{res}")
     finally:
         driver.quit()
